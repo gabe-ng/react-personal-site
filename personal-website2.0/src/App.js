270017edd62b9c20f83c8da2968c0570417e291f
Gabriel Ng <gabrielng15@gmail.com>
2018-08-07 12:42:02 -0700

    started creating react files

--- personal-website2.0/src/App.js
+++ personal-website2.0/src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <div id="background" className="background" />
+        <header id="top">
+          <div class="heading-wrap">
+            <h1>
+              hi, i'm
+              <span class="orange">Gabriel Ng</span>
+            </h1>
+            <h1>i'm a fullstack web developer</h1>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*<MainContainer />
+    <Footer />*/}
       </div>
     );
   }
