f36af71f158a53b3e3015207afcdcdab81418c8d
Gabriel Ng <gabrielng15@gmail.com>
2018-08-13 13:58:57 -0700

    adding key frames

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,28 +5,45 @@ import "./Header.css";
 
 class Header extends Component {
   componentDidMount = () => {
-    return;
-    // write fade in/out
+    const w = window;
+    let intro = document.getElementById("intro");
+    let link = document.getElementById("title-link");
+    let pos = 250;
+
+    w.onscroll = () => {
+      if (window.pageYOffset > pos) {
+        intro.classList.add("hidden");
+        intro.classList.remove("fadeinDown");
+
+        link.classList.remove("hidden");
+        link.classList.add("fadeinUp");
+      } else if (window.pageYOffset < pos) {
+        intro.classList.remove("hidden");
+        intro.classList.add("fadeinDown");
+
+        link.classList.add("hidden");
+        link.classList.remove("fadeinUp");
+      }
+    };
   };
 
   render() {
     return (
       <div className="header-wrap">
-        <section id="intro" className="intro">
+        <section id="intro" className="intro fadeinDown">
           <h1 className="main-title">
             Hi, I'm
             <span> Gabriel Ng</span>
           </h1>
           <h1 className="second-title slidingVertical">
-            I'm a
-            <span> fullstack web developer</span>
+            I'm a<span> fullstack web developer</span>
             <span> dog lover</span>
             <span> software engineer</span>
             <span> board game enthusiast</span>
             <span> quick learner</span>
           </h1>
         </section>
-        <header id="title-link" className="">
+        <header id="title-link" className="hidden">
           <Link className="title-link" to="/">
             Gabriel{" "}
           </Link>
--- src/MainContainer/NavBar/NavBar.js
+++ src/MainContainer/NavBar/NavBar.js
@@ -4,22 +4,40 @@ import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 const Navbar = () => {
-  return <div className="main-nav">
+  return (
+    <div className="main-nav">
       <ul className="links">
-        <NavLink className="nav-item" activeClassName="active-nav-item" to="/about">
-            About
+        <NavLink
+          className="nav-item"
+          activeClassName="active-nav-item"
+          to="/projects"
+        >
+          Projects
         </NavLink>
-        <NavLink className="nav-item" activeClassName="active-nav-item" to="/projects">
-            Projects
+        <NavLink
+          className="nav-item"
+          activeClassName="active-nav-item"
+          to="/skills"
+        >
+          Skills
         </NavLink>
-        <NavLink className="nav-item" activeClassName="active-nav-item" to="/skills">
-            Skills
+        <NavLink
+          className="nav-item"
+          activeClassName="active-nav-item"
+          to="/about"
+        >
+          About
         </NavLink>
-        <NavLink className="nav-item" activeClassName="active-nav-item" to="/contact">
-            Contact
+        <NavLink
+          className="nav-item"
+          activeClassName="active-nav-item"
+          to="/contact"
+        >
+          Contact
         </NavLink>
       </ul>
-    </div>;
+    </div>
+  );
 };
 
 export default Navbar;
